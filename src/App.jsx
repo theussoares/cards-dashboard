ce017983d4b1edeccdbc518e6d5648ddb14d9cb8
Matheus Ferreira Soares <matheusfs774@gmail.com>
2025-09-19 19:07:31 -0400

    feat: first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Header from "./components/Header"
+
+function App() {
+
+  return (
+    <html lang="en">
+      <header>
+        <Header />
+      </header>
+
+      <main></main>
+  
+      <footer></footer>
+    </html>
+  )
+}
+
+export default App
